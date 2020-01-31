4f73a0060bd3144d5ebbbf7a6534eb8fb20e1291
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-01-31 13:09:05 -0800

    factorial function prototype

--- src/classwork/03_assign/loops.cpp
+++ src/classwork/03_assign/loops.cpp
@@ -8,5 +8,21 @@ factorial(5);
 
 returns 120
 
+
+
+
 DON'T FORGET TO WRITE TEST CASE.  See file loop_test.cpp
 */
+
+void factorial(int num)
+{
+	int total = 1;
+	for (int i = 1; i < num + 1; i++)
+	{
+
+		total = i * total;
+		cout << i << endl;
+		cout << total << endl;
+
+	}
+}
