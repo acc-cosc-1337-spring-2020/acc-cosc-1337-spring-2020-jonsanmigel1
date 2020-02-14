023d6f9ac5dc4229930acaa24af521af9a527525
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-02-13 18:31:05 -0600

    ref,value and const ref param

--- src/examples/03_module/05_value_and_reference_params/main.cpp
+++ src/examples/03_module/05_value_and_reference_params/main.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+using std::cout;
 
 int main() 
 {
+	int num = 5;
+	cout << "Value  of num is : " << num<< "Address is" << &num;
+
+	//Create a reference variable
+	int &num_ref = num;
+
+	//you now have access to the memory so you can change the number stored
+	num_ref = 10;
+	cout << "\nValue  of num is : " << num << "Address is" << &num;
+
+
+	//Cout will display 10 not 5 anymore 
 
 	return 0;
 }
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "while.h"
+#include "value_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -12,6 +13,20 @@ TEST_CASE("Test sum of sqwuares function ")
 
 	REQUIRE(sum_of_squares(3) == 14);
 	REQUIRE(sum_of_squares(4) == 30);
+
+
+}
+
+TEST_CASE("Test pass by value and ref function")
+{
+	int num1 = 5, num2 = 10;
+
+	pass_by_val_and_ref(num1, num2);
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num2 == 50);
+
+
 
 
 }
