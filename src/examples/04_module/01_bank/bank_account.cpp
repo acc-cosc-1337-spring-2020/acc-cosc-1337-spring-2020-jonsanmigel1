21b0cc73977b3c0e8423e1fc9ec15afa74a84112
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-02-25 19:07:33 -0600

    Working on bank

--- src/examples/04_module/01_bank/bank_account.cpp
+++ src/examples/04_module/01_bank/bank_account.cpp
@@ -1,1 +1,24 @@
+#include "bank_account.h"
+
 //bank_account.cpp
+
+void BankAccount::deposit(int amount)
+{
+	if (amount > 0)
+	{
+		balance += amount;
+		//We know we want to modify balance so we dont declare const
+	}
+
+}
+
+void BankAccount::withdraw(int amount)
+{
+	if (amount > 0 && amount <= balance)
+	{
+		balance -= amount;
+	}
+
+
+}
+
--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,1 +1,15 @@
 //bank_account.h
+class BankAccount
+{
+public:
+	BankAccount(int b) : balance{ b } { }
+	int get_balance()const { return balance; }
+	void deposit(int amount); //create deposit prototype 
+	void withdraw(int amount);
+
+private:
+	int balance;
+
+
+
+};
