c09f8612ffe7eaa3992285c7416211f14c36cfaa
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-03-31 00:23:47 -0700

    Got Player X and O working

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -6,9 +6,12 @@ int main()
 {
 	string firstplayer;
 	int value;
+	int count=0;
 	string PlayerXorO;
 	string choice;
+	string the_current_player;
 	
+
 	//Only prompt user once For X or O to start game
 	std::cout << "Please Enter X or O:  ";
 	cin >> PlayerXorO;
@@ -16,18 +19,33 @@ int main()
 	//Start the game and send PlayerXorO as argument
 	TicTacToe thegame;
 	thegame.start_game(PlayerXorO);
+	
 
 	do
 	{
-		firstplayer = thegame.get_player();
-		cout << "Player " << firstplayer << "Enter from 1-9:  "<<"\n";
-		cin >> value;
-		
+
+		if (PlayerXorO == "X")
+		{
+			std::cout << "PLayer " << PlayerXorO << "Please enter 1-9" << std::endl;
+			cin >> value;
+
+			
+			PlayerXorO = "O";
+		}
+		else if (PlayerXorO == "O")
+		{
+			std::cout << "PLayer " << PlayerXorO << "Please enter 1-9" << std::endl;
+			cin >> value;
+			
+			PlayerXorO = "X";
+
+		}
+
 		try
 		{
 			thegame.mark_board(value);
 		}
-
+	
 
 
 		catch (Error the_error)
@@ -35,14 +53,15 @@ int main()
 			cout << the_error.get_message();
 		}
 
-			
-			
-			
-		cout << "WOuld you like to continue? Type Y to continue or N to exit"<<"\n";
+
+
+
+		cout << "WOuld you like to continue? Type Y to continue or N to exit" << "\n";
 		cin >> choice;
 
 
-	} while (choice=="Y");
+	} while (choice == "Y");
+
 	if (choice == "N")
 	{
 		exit(8);
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -11,6 +11,8 @@ public:
 	void start_game(string first_player);
 	int mark_board(int position);
 	
+	void set_next_player();
+	
 	string get_player() const { return next_player; }
 	string get_winner() { return winner; }
 	
@@ -29,7 +31,7 @@ private:
 	string winner;
 	string player;
 	string next_player;
-	void set_next_player();
+	//void set_next_player();
 	std::vector<std::string>pegs{ 9, " " };
 
 };
