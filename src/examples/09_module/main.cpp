912d0729e6d1fcb61a067ec568d4b5b7b83d322e
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-04-16 15:12:45 -0700

    example 9

--- src/examples/09_module/main.cpp
+++ src/examples/09_module/main.cpp
@@ -1,6 +1,18 @@
-
+#include "temperature_data.h"
+#include <iostream>
 int main() 
 {
+	std::vector<Temperature> out_temps{ Temperatur mperature(2,99.5),Temperature(3.99) };
+
+	;
+	data.save_temps(out_teps);
+
+	std::vector<Temperature> in_temps = data.get_temps();
+
+	for (auto temp : in_temps)
+	{
+		std::cout << temp.get_hour() << " " << tempet_reading() << "\n";
+	}
 
 	return 0;
 }
