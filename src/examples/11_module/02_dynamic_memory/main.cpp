c658f3be90e050cdaf1459f20a2b90df2d186913
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-04-16 16:56:08 -0700

    Dynamic memory example

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,17 @@
+
+#include <iostream>
+using std::cout;
+
 int main() 
 {
+	int *ptr_num = new int(5);
+	cout << "address of ptr_num:  " << &ptr_num << "\n";
+	cout << " Address ptr_num points to:  " << ptr_num << "\n";
+	cout << "Value at address ptr_num points to:  " << *ptr_num;
+
+	delete ptr_num;
+	ptr_num = nullptr;
+	cout << ptr_num;
 	
 	return 0;
 }
