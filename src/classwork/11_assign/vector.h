13bb50c396884114b4eac35bd46642e8d8309a33
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-04-23 19:07:35 -0700

    Vector memory management

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,2 +1,24 @@
-//
-//TODO 7 add template
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+#include<cstddef>
+#include<stddef.h>
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v);//copy constructor- Rule of 3
+	Vector& operator=(const Vector& v);//copy assignment-Rule of 3
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i)const { return nums[i]; }
+	~Vector();//Destructor-Rule of 3
+private:
+	size_t size;
+	int* nums;
+};
+
+#endif // !1
+
+// doesnt belong to class / free functon
+void use_vector();
--- test/classwork_test/11_assign_test/vector_tests.cpp
+++ test/classwork_test/11_assign_test/vector_tests.cpp
@@ -6,3 +6,21 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Tets class copy with stack variables")
+{
+	Vector v1(3);
+	Vector v2 = v1;
+
+	REQUIRE(v1.Size() == v2.Size());
+
+
+}
+TEST_CASE("Test class copy dynamic heap with 2 varables")
+{
+	Vector v1(2);
+	Vector v2(3);
+
+	v2 = v1;
+	v1[1] = 5;
+	REQUIRE(v1[1] == v2[2]);
+}
