58113ff9fd7b9e29a375312ddb426394199471f8
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-04-28 02:10:29 -0700

    Working on TicTacToeData

--- src/homework/tic_tac_toe/tic_tac_toe_data.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_data.cpp
@@ -1,1 +1,38 @@
+#include "tic_tac_toe_data.h"
+
 //cpp
+
+void TicTacToeData::save_pegs(const std::vector<TicTacToe>& pgs)
+{
+	std::ofstream file_out(file_name, std::ios_base::trunc);
+	//std::vector<TicTacToe>::size_type i = 0; i != pgs.size(); i++
+	for (auto vec : pgs)
+	{
+		
+		for (auto character : pgs)
+		{
+			file_out << character;
+		}
+		file_out << vec.get_winner();
+		std::cout << "\n";
+	}
+}
+
+std::vector<std::unique_ptr<TicTacToe>> TicTacToeData::get_games()
+{
+	std::unique_ptr<TicTacToe> boards;
+	std::ifstream open_file(file_name); //create file handle object
+
+	while (open_file.is_open())
+	{
+		std::vector<std::string> astring;
+		std::string line;
+
+		while (open_file >>line)
+		{
+			std::cout << "Hellp";
+		}
+	}
+
+	return std::vector<std::unique_ptr<TicTacToe>>();
+}
--- src/homework/tic_tac_toe/tic_tac_toe_data.h
+++ src/homework/tic_tac_toe/tic_tac_toe_data.h
@@ -1,1 +1,25 @@
 //h
+#ifndef TIC_TAC_TOE_DATA_H
+#define TIC_TAC_TOE_DATA_H
+#include <vector>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <iostream>
+using std::unique_ptr;
+using std::make_unique;
+#include "tic_tac_toe.h"
+class TicTacToeData
+{
+public:
+	void save_pegs(const std::vector<TicTacToe>& pgs);
+	std::vector<std::unique_ptr<TicTacToe>> get_games();
+
+private:
+	std::string file_name = { "TicTacToeData.dat" };
+
+};
+
+
+
+#endif // !TIC_TAC_TOE_DATA_H
