f8b776e1f25b2f0de507ece96556b10e213da8f6
Jon San Migel <jon.sanmigel@g.austincc.edu>
2020-04-29 00:52:24 -0700

    working on homework

--- src/homework/tic_tac_toe/tic_tac_toe_data.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_data.cpp
@@ -1,19 +1,19 @@
 #include "tic_tac_toe_data.h"
-
+#include "tic_tac_toe_3.h"
 //cpp
 
-void TicTacToeData::save_pegs(const std::vector<TicTacToe>& pgs)
+void TicTacToeData::save_pegs(const std::vector<std::unique_ptr<TicTacToe>>& pgs)
 {
 	std::ofstream file_out(file_name, std::ios_base::trunc);
-	//std::vector<TicTacToe>::size_type i = 0; i != pgs.size(); i++
-	for (auto vec : pgs)
+	
+	for (auto &vec : pgs)
 	{
 		
-		for (auto character : pgs)
+		for (auto &character : pgs)
 		{
-			file_out << character;
+			file_out << *character;
 		}
-		file_out << vec.get_winner();
+		file_out << vec->get_winner();
 		std::cout << "\n";
 	}
 }
@@ -25,13 +25,26 @@ std::vector<std::unique_ptr<TicTacToe>> TicTacToeData::get_games()
 
 	while (open_file.is_open())
 	{
-		std::vector<std::string> astring;
+		std::vector<std::string> pegs;
 		std::string line;
 
 		while (open_file >>line)
 		{
-			std::cout << "Hellp";
-		}
+			for (int ch = 0; ch<line.size()-1; ch++)
+			{
+				std::string chstring(1, line[ch]);
+				pegs.push_back(chstring);
+			}
+			std::string winner = pegs[-1];// last item in the vector of string
+			std::unique_ptr<TicTacToe> board;
+
+			if (pegs.size() == 9)
+			{
+				boards = std::make_unique<TicTacToe3>();
+			}
+
+		}	
+
 	}
 
 	return std::vector<std::unique_ptr<TicTacToe>>();
